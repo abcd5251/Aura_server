53d429901b1ebde6b7bca7a33bcc9f86669adcd9
allen <74865251a@gmail.com>
2025-04-05 20:51:32 +0800

    update prompt

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ app.add_middleware(
 @app.post("/defiInfo")
 async def process_simple_input(data: InputData):
     need_info = INFORMATION
-    qa_model_instance = OpenAIModel(system_prompt="You are a helpful assistant, based on the INFORMATION below, Please answer the QUESTION!", temperature=0)
+    qa_model_instance = OpenAIModel(system_prompt="You are a helpful assistant, based on the diamond INFORMATION below, Please answer the QUESTION!", temperature=0)
     prompt = f"INFORMATION:{need_info}\nQUESTION:{data.input_text}\nOUTPUT:"
     output, input_token, output_token = qa_model_instance.generate_string_text(prompt)
 
