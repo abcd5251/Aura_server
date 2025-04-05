8785342272d2014250b1e3b485c6aa8a261dd7dc
allen <74865251a@gmail.com>
2025-04-05 20:47:40 +0800

    add diamond

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,120 @@
 COLLECTION_NAME = "DeFi_Knowledge"
 EMBEDDING_MODEL = "text-embedding-3-small"
 FILE_PATH = "./data/information.txt"
+INFORMATION="""
+AuraGem #001
+Asset ID: #1
+
+Type: Ruby
+
+Cut: Round Brilliant
+
+Asset Value
+
+2.5 ETH
+
+Interest Rate
+
+8.00% APR
+
+Loan-to-Value
+
+70%
+
+Max Loan Amount
+
+1.75 ETH
+
+Lending Terms
+• Loan Duration: 30 days
+• Repayment Schedule: Full amount at maturity
+• Early Repayment: Allowed with no penalties
+• Liquidation Threshold: 85% of initial value
+==============================================
+AuraGem #006
+Asset ID: #6
+
+Type: Citrine
+
+Cut: Princess Cut
+
+Asset Value
+
+1.9 ETH
+
+Interest Rate
+
+9.00% APR
+
+Loan-to-Value
+
+70%
+
+Max Loan Amount
+
+1.33 ETH
+
+Lending Terms
+• Loan Duration: 30 days
+• Repayment Schedule: Full amount at maturity
+• Early Repayment: Allowed with no penalties
+• Liquidation Threshold: 85% of initial value
+=============================================
+AuraGem #011
+Asset ID: #11
+
+Type: Sapphire
+
+Cut: Emerald Cut
+
+Asset Value
+
+3.2 ETH
+
+Interest Rate
+
+7.50% APR
+
+Loan-to-Value
+
+70%
+
+Max Loan Amount
+
+2.24 ETH
+
+Lending Terms
+• Loan Duration: 30 days
+• Repayment Schedule: Full amount at maturity
+• Early Repayment: Allowed with no penalties
+• Liquidation Threshold: 85% of initial value
+==========================================
+AuraGem #016
+Asset ID: #16
+
+Type: Emerald
+
+Cut: Oval
+
+Asset Value
+
+2.5 ETH
+
+Interest Rate
+
+8.50% APR
+
+Loan-to-Value
+
+70%
+
+Max Loan Amount
+
+1.75 ETH
+
+Lending Terms
+• Loan Duration: 30 days
+• Repayment Schedule: Full amount at maturity
+• Early Repayment: Allowed with no penalties
+• Liquidation Threshold: 85% of initial value
+"""
